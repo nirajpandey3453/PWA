0563a9514d4c5e3b26be3495f159f5df0ca868a1
Niraj Pandey <144918430+nirajpandey3453@users.noreply.github.com>
2026-08-29 19:59:47 +0530

    dd

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sip-tracker-v32';
+const CACHE = 'sip-tracker-v33';
 const ASSETS = ['./', './index.html', './manifest.json',
                 './icons/favicon-32.png', './icons/icon-192.png'];
 
